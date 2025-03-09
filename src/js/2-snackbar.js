350ac1f06921363b779fef882affdc94343b034d
Vik-ing-ing <Dakraman1232@ukr.net>
2025-03-09 19:15:33 +0200

    1

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,4 +1,7 @@
-import iziToast from "izitoast/dist/js/iziToast.min.js";
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+
+import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
 
